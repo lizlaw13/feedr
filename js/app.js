633bc2f7660b8b096d0b9b7c3e08e660019625be
liz lawrence <elizabeth.law13@gmail.com>
2019-04-09 21:21:51 -0700

    Added new api source and condensed js file

--- js/app.js
+++ js/app.js
@@ -1,97 +1,113 @@
-/*
-  Please add all Javascript code to this file.
-*/
-let apiKey = '7934a4fdec7d43b4b539fa21aa4592e3';
-
-var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=7934a4fdec7d43b4b539fa21aa4592e3';
 // retreiving api
-$.get(url).then(function(data){
-    console.log('data:', data)
-    // template for title and featured picture 
-    let articleContent = ``;
-    data.articles.forEach(function(article){
 
-      // use data attributes - thank you jessica she hella helped me 
-      articleContent +=  `
-      <article class="article" data-title="${article.title}" data-description="${article.description}" data-link="${article.url}" data-source="${article.source[1]}">
+let apiKey = "7934a4fdec7d43b4b539fa21aa4592e3";
+
+var url =
+  "https://newsapi.org/v2/top-headlines?" + "country=us&" + "apiKey=" + apiKey;
+
+$.get(url).then(function(data) {
+  console.log("data:", data);
+  // template for title and featured picture
+  let articleContent = ``;
+
+  data.articles.forEach(function(article) {
+    let author;
+    if (article.author == null) {
+      author = "Author Not Found";
+    } else {
+      author = article.author;
+    }
+    articleContent += `
+      <article class="article" data-title="${
+        article.title
+      }" data-description="${article.description}" data-link="${
+      article.url
+    }" data-source="${article.source[1]}">
       <section class="featuredImage">
         <img src="${article.urlToImage}"alt="" />
       </section>
       <section class="articleContent">
           <a href="#"><h3>${article.title}</h3></a>
-          <h6>${article.author}</h6>
-      </section>
-      <section class="impressions">
-        526
+          <h6>${author}</h6>
       </section>
       <div class="clearfix"></div>
       </article> 
-      `
-    });
+      `;
+  });
 
-    // need parent of container and add template to the html element 
-    $('#main').html(articleContent);
+  // need parent of container and add template to the html element
+  $("#main").html(articleContent);
 });
-
-
 
 // click on title to show pac man loader, hide pac man loader, and insert title, description, and link
-$('#main').on('click','.article', function() {
-  console.log('this is running')
-  $('#popUp').removeClass('hidden');
-  $('#popUp').removeClass('loader');
+$("#main").on("click", ".article", function() {
+  $("body").css("overflow", "hidden");
+  $("#popUp").removeClass("hidden");
+  $("#popUp").removeClass("loader");
 
-// create variables for each data attribute
-  let title = $(this).data('title');
-  let description = $(this).data('description');
-  let link = $(this).data('link');
-  $('.container > h1').text(title);
-  $('.container > p').text(description);
-  $('.container > a').attr('href', link);
+  // create variables for each data attribute
+  let title = $(this).data("title");
+  let description = $(this).data("description");
+  let link = $(this).data("link");
+  $(".container > h1").text(title);
+  $(".container > p").text(description);
+  $(".container > a").attr("href", link);
 });
 
+// click on x to get rid of the modal
+$(".closePopUp").on("click", function() {
+  $("#popUp").addClass("hidden");
+  $("body").css("overflow", "auto");
+});
 
 //click search icon to expand input
-$('#search').on('click','img', function(){
-  $('#search').toggleClass('active');
+$("#search").on("click", "img", function() {
+  $("#search").toggleClass("active");
 });
 
-
-
-// click on x to get rid of the modal 
-$('.closePopUp').on("click",function(){
-  $('#popUp').addClass('hidden');
+$("#source1").on("click", function() {
+  getArticleContent(
+    "https://newsapi.org/v2/everything?sources=buzzfeed&from=2019-04-09&to=2019-04-09&sortBy=popularity&apiKey=7934a4fdec7d43b4b539fa21aa4592e3"
+  );
 });
 
-// integrate new API and populate on click but the article on click does not work because i did not do it
-$('#source1').on('click', function(){
-url2 = 'https://content.guardianapis.com/search?api-key=25e5a0d6-5139-46a6-9214-da95627cc11e'
+// generate article content for each api being passed in
+function getArticleContent(url) {
+  $.get(url).then(function(data) {
+    console.log("data", data);
 
-$.get(url2).then(function(data2){
-  console.log('data:', data2)
-  console.log('hey!' , data2.response.results);
-
-  let articleContentTwo = ``;
-    data2.response.results.forEach(function(r){
-      articleContentTwo +=  `
-      <article class="article">
+    let articleContent = ``;
+    data.articles.forEach(function(article) {
+      let author;
+      if (article.author == null) {
+        author = "Author Not Found";
+      } else {
+        author = article.author;
+      }
+      let title;
+      if (
+        article.title !=
+          "WTF Is Happening Today? BuzzFeed Newsletters Can Help!" ||
+        author != "Tanner Greenring"
+      ) {
+        articleContent += `
+      <article class="article" data-title="${
+        article.title
+      }" data-description="${article.description}" data-link="${
+          article.url
+        }" data-source="${article.source[1]}">
       <section class="featuredImage">
-        <img src="${r.sectionName}"alt="" />
+        <img src="${article.urlToImage}"alt="" />
       </section>
       <section class="articleContent">
-          <a href="#"><h3>${r.webTitle}</h3></a>
-          <h6>${r.pillarName}</h6>
-      </section>
-      <section class="impressions">
-        526
+          <a href="#"><h3>${article.title}</h3></a>
+          <h6>${author}</h6>
       </section>
       <div class="clearfix"></div>
       </article> 
-      `
-});
-$('#main').html(articleContentTwo);
-});
-});
-
+      `;
+      }
+    });
+    $("#main").html(articleContent);
+  });
+}
