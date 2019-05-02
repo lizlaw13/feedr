504b26a27bace1cf9d3c81c248a2fa34c0cbb6cf
liz lawrence <elizabeth.law13@gmail.com>
2019-05-02 12:39:12 -0700

    Cleaned up API calls- added more sources

--- js/app.js
+++ js/app.js
@@ -1,44 +1,3 @@
-// retreiving api
-
-let apiKey = "7934a4fdec7d43b4b539fa21aa4592e3";
-
-var url =
-  "https://newsapi.org/v2/top-headlines?" + "country=us&" + "apiKey=" + apiKey;
-
-$.get(url).then(function(data) {
-  console.log("data:", data);
-  // template for title and featured picture
-  let articleContent = ``;
-
-  data.articles.forEach(function(article) {
-    let author;
-    if (article.author == null) {
-      author = "Author Not Found";
-    } else {
-      author = article.author;
-    }
-    articleContent += `
-      <article class="article" data-title="${
-        article.title
-      }" data-description="${article.description}" data-link="${
-      article.url
-    }" data-source="${article.source[1]}">
-      <section class="featuredImage">
-        <img src="${article.urlToImage}"alt="" />
-      </section>
-      <section class="articleContent">
-          <a href="#"><h3>${article.title}</h3></a>
-          <h6>${author}</h6>
-      </section>
-      <div class="clearfix"></div>
-      </article> 
-      `;
-  });
-
-  // need parent of container and add template to the html element
-  $("#main").html(articleContent);
-});
-
 // click on title to show pac man loader, hide pac man loader, and insert title, description, and link
 $("#main").on("click", ".article", function() {
   $("body").css("overflow", "hidden");
@@ -65,12 +24,6 @@ $("#search").on("click", "img", function() {
   $("#search").toggleClass("active");
 });
 
-$("#source1").on("click", function() {
-  getArticleContent(
-    "https://newsapi.org/v2/everything?sources=buzzfeed&from=2019-04-09&to=2019-04-09&sortBy=popularity&apiKey=7934a4fdec7d43b4b539fa21aa4592e3"
-  );
-});
-
 // generate article content for each api being passed in
 function getArticleContent(url) {
   $.get(url).then(function(data) {
@@ -84,12 +37,7 @@ function getArticleContent(url) {
       } else {
         author = article.author;
       }
-      let title;
-      if (
-        article.title !=
-          "WTF Is Happening Today? BuzzFeed Newsletters Can Help!" ||
-        author != "Tanner Greenring"
-      ) {
+      {
         articleContent += `
       <article class="article" data-title="${
         article.title
@@ -111,3 +59,27 @@ function getArticleContent(url) {
     $("#main").html(articleContent);
   });
 }
+
+$("#source0").on("click", function() {
+  getArticleContent(
+    "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=7934a4fdec7d43b4b539fa21aa4592e3"
+  );
+});
+
+$("#source1").on("click", function() {
+  getArticleContent(
+    "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=7934a4fdec7d43b4b539fa21aa4592e3"
+  );
+});
+
+$("#source2").on("click", function() {
+  getArticleContent(
+    "https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=7934a4fdec7d43b4b539fa21aa4592e3"
+  );
+});
+
+$("#source3").on("click", function() {
+  getArticleContent(
+    "https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=7934a4fdec7d43b4b539fa21aa4592e3"
+  );
+});
